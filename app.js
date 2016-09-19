d6d94684a4b440ab9475c4655e5dd039eb785c84
Vikas Sharma <vikas3.sharma@bmo.com>
2016-09-18 21:37:20 -0400

    changes;

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 /**
  * Created by vshr on 2016-09-17.
  */
+(function () {
+  'use strict';
+    angular.module('NameCalculator',[])
+        .controller('NameCalculatorController', function ($scope,$filter) {
+            $scope.name ="";
+            $scope.finalValue =0;
+            $scope.upperName="";
+
+            $scope.upper =  function () {
+                var upCase = $filter('uppercase');
+                $scope.upperName = upCase($scope.name);
+            }
+
+            $scope.dispayNumeric= function(){
+                $scope.finalValue = convertCharToString($scope.name);
+            }
+
+        function convertCharToString(string){
+            var totalStringValue =0 ;
+            for(var i=0; i < string.length;i++){
+                totalStringValue += string.charCodeAt(i);
+            }
+            return totalStringValue;
+        }
+        });
+})();
